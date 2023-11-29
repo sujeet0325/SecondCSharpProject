96ff2c569c89e9cfd0aacf7126e78512f6c59d3f
sujeet0325 <142496120+sujeet0325@users.noreply.github.com>
2023-11-29 15:36:02 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondCSharpProject
+{
+    internal class Program
+    {
+        public void Square()
+        {
+            int a = 10,c;
+            c = a * a;
+            Console.WriteLine("Square = " + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p= new Program();
+            p.Square();
+            Console.ReadLine();
+        }
+    }
+}
